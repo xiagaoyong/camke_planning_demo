d8849cc3f8418e199db9783b03ed7f1e7145d3db
Alan <1435582884@qq.com>
2024-04-07 22:19:16 +0800

    basic mode

---
+++ src/process/process.cpp
@@ -0,0 +1,8 @@
+#include "process.h"
+#include <iostream>
+void Process::planProcess() {
+
+  std::cout << "this is planning process" << std::endl;
+  _my_map.mapInfo();
+  std::cout << "plainning success!" << std::endl;
+}
